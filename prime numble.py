2e2d554c94f1ec1ac04512f6518843a02d9d86a9
kyrali <42790007+kyrali@users.noreply.github.com>
2018-10-31 09:32:34 +0800

    Add files via upload

---
+++ prime numble.py
@@ -0,0 +1,18 @@
+n=eval(input("how many prime numble in"))
+a=[2]
+d=[]
+for i in range (3,n+1):
+    b=0
+    c=int(i/2)+1
+    for m in a:
+        if c>=m:
+            d.append(m)
+        for j in d:
+            if i%j==0:
+                break
+            else:
+                b=b+1
+        if b==len(d):
+            a.append(i)
+print(a)
+            
