1f6557295cb2b304a285998a7551649c63c8ac39
kyrali <42790007+kyrali@users.noreply.github.com>
2018-11-07 09:32:20 +0800

    Add files via upload

---
+++ Manager.java
@@ -0,0 +1,13 @@
+
+public class Manager {
+
+	public Manager() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Tracy is too tall");
+
+	}
+
+}
